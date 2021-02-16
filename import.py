d6db53c1631e028a1d9f57778740d76c9183188b
y9ning <yuankai.ning@ucalgary.ca>
2021-02-15 22:43:01 -0500

    templates, import, and csv added

---
+++ import.py
@@ -0,0 +1,31 @@
+import os
+import csv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+engine= create_engine(os.getenv("DATABASE_URL"))
+db =scoped_session(sessionmaker(bind=engine))
+
+def main():
+    f=open("books.csv")
+    reader =csv.reader(f)
+    db.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR NOT NULL, password VARCHAR NOT NULL)")
+    db.execute("CREATE TABLE books (isbn VARCHAR PRIMARY KEY,title VARCHAR NOT NULL,author VARCHAR NOT NULL,year VARCHAR NOT NULL)")
+    db.execute("CREATE TABLE reviews (isbn VARCHAR NOT NULL,username VARCHAR NOT NULL,review VARCHAR NOT NULL, rating INTEGER NOT NULL)")
+
+    for isbn,title,author,year in reader:
+        if year == "year":
+            print('this is header')
+        else:
+            db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn,:title,:author,:year)",{"isbn":isbn,"title":title,"author":author,"year":year})
+
+    print("books added to the database")
+    db.commit()
+
+
+if __name__ == "__main__":
+    main()
